582246056398c9e72403afd63632ff83179b4181
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-13 22:50:49 +0100

    event listener on search btn js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = "03a285d67f4bed7d17c49d39f2b39a47";
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -9,15 +9,22 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkweather(city) {
   const response = await fetch(apiURL + city + `&appid=${apiKey}`);
 
-  if(response.status == 404){
-    document.querySelector("weather").style.display = "none";
-  }else{
+  if (response.status === 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
     let data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
   }
-  document.querySelector("weather").style.display = "block";
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".error").style.display = "none";
 }
+
+searchBtn.addEventListener("click", () => {
+  checkweather(searchBox.value);
+});
