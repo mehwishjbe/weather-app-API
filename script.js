afe9e7ff9b71fce725f7832ef9796edbe0be26a2
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-13 22:29:48 +0100

    response condition added js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const apiKey = "03a285d67f4bed7d17c49d39f2b39a47";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkweather(city){
-    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+async function checkweather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+
+  if(response.status == 404){
+    document.querySelector("weather").style.display = "none";
+  }else{
+    let data = await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
+  }
+  document.querySelector("weather").style.display = "block";
 }
