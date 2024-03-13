ef58f6542c1b63be1c33b11c06ffb17cf3ee6547
mehwishjbe <mehwish.jbe@gmail.com>
2024-03-13 22:18:25 +0100

    search field accessed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const apiKey = "03a285d67f4bed7d17c49d39f2b39a47";
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
